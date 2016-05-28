17c593a01d5e1702306dbe4755963cbbc91143bb
RuchiraPadave <ruchirawohlig@gmail.com>
2016-05-28 11:32:29 +0530

    added page

--- js/controllers.js
+++ js/controllers.js
@@ -11,10 +11,8 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.navigation = NavigationService.getnav();
 
   $scope.mySlides = [
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+    'img/home/slider1.png',
+    // 'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
   ];
 })
 
