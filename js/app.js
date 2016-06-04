450ec168bd6c50e41c38f6c92c3f9efa93f6aae2
TusharSachde <tushar@wohlig.com>
2016-06-04 12:34:26 +0530

    Img added

--- js/app.js
+++ js/app.js
@@ -68,6 +68,20 @@ firstapp.directive('fancyboxBox', function($document) {
     };
 });
 
+firstapp.directive('autoHeight', function($compile, $parse) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            var windowHeight = $(window).height();
+            var addHeight = function() {
+                $element.css("min-height", windowHeight);
+            };
+            addHeight();
+        }
+    };
+});
 
 firstapp.config(function ($translateProvider) {
   $translateProvider.translations('en', LanguageEnglish);
