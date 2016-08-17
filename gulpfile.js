6f97f27b87ee64656b78f4b1af0d10d5986c1ed5
Ruchiwohlig <ruchirawohlig@gmail.com>
2016-08-17 19:51:02 +0530

    Okay

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var jsArray = [
     './bower_components/angulartics/dist/angulartics.min.js',
     './bower_components/jStorage/jstorage.min.js',
     './bower_components/angulartics-google-analytics/dist/angulartics-google-analytics.min.js',
+    
     './js/app.js',
     './js/language.js',
     './js/controllers.js',
