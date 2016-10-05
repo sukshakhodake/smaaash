63db3a0b0b715ba8ff19a06058772875f137bbe0
sukshawohlig <sukshawohlig@gmail.com>
2016-10-05 18:07:46 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -518,3 +518,14 @@ firstapp.directive('aplhaOnly', function() {
 firstapp.filter('urlEncode', [function() {
     return window.encodeURIComponent;
 }]);
+
+firstapp.filter('dateSuffix', function($filter) {
+  var suffixes = ["th", "st", "nd", "rd"];
+  return function(input) {
+    var dtfilter = $filter('date')(input, 'MMMM dd');
+    var day = parseInt(dtfilter.slice(-2));
+    var relevantDigits = (day < 30) ? day % 20 : day % 30;
+    var suffix = (relevantDigits <= 3) ? suffixes[relevantDigits] : suffixes[0];
+    return dtfilter+suffix;
+  };
+});
